9ce40160d01f11d0c85bc9fcca20a7e4e519944c
Tayfun Gürlevik <tayfun.gurlevik@hacettepe.edu.tr>
2022-10-12 11:41:21 +0300

    converting trc files done. Started to bvh file conversion

---
+++ trc parser/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace trc_parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trcRadioButton = new System.Windows.Forms.RadioButton();
+            this.bvhradiobutton = new System.Windows.Forms.RadioButton();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selected Folder:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(14, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 354);
+            this.listBox1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.statusLabel,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 444);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 32);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(64, 25);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 25);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 24);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "to .csv";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trcRadioButton
+            // 
+            this.trcRadioButton.AutoSize = true;
+            this.trcRadioButton.Checked = true;
+            this.trcRadioButton.Location = new System.Drawing.Point(146, 51);
+            this.trcRadioButton.Name = "trcRadioButton";
+            this.trcRadioButton.Size = new System.Drawing.Size(97, 29);
+            this.trcRadioButton.TabIndex = 5;
+            this.trcRadioButton.TabStop = true;
+            this.trcRadioButton.Text = ".trc files";
+            this.trcRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // bvhradiobutton
+            // 
+            this.bvhradiobutton.AutoSize = true;
+            this.bvhradiobutton.Location = new System.Drawing.Point(294, 51);
+            this.bvhradiobutton.Name = "bvhradiobutton";
+            this.bvhradiobutton.Size = new System.Drawing.Size(107, 29);
+            this.bvhradiobutton.TabIndex = 6;
+            this.bvhradiobutton.Text = ".bvh files";
+            this.bvhradiobutton.UseVisualStyleBackColor = true;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.bvhradiobutton);
+            this.Controls.Add(this.trcRadioButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = ".trc parser";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton trcRadioButton;
+        private System.Windows.Forms.RadioButton bvhradiobutton;
+    }
+}
+
---
+++ trc parser/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace trc_parser
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        bool bvh = false;
+        bool trc = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateFileExtensionConditions();
+            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                path= folderBrowserDialog1.SelectedPath;
+                label2.Text = path;
+                DirectoryInfo dinfo = new DirectoryInfo(path);
+                listBox1.Items.Clear();
+                foreach (var file in dinfo.GetFiles())
+                {
+                    if (trc)
+                    {
+                        if (file.Extension == ".trc")
+                        {
+                            listBox1.Items.Add(file.Name);
+                        }
+                    }
+                    else
+                    {
+                        if (file.Extension == ".bvh")
+                        {
+                            listBox1.Items.Add(file.Name);
+                        }
+                    }
+                    
+                }
+                statusLabel.Text = "Found " + listBox1.Items.Count + " files.";
+            }
+            
+        }
+
+        private void UpdateFileExtensionConditions()
+        {
+            trc = trcRadioButton.Checked;
+            bvh = bvhradiobutton.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int itemCount = listBox1.Items.Count;
+            int counter = 1;
+            UpdateFileExtensionConditions();
+            if (trc)
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    ConvertToCSV(item.ToString());
+                    toolStripProgressBar1.ProgressBar.Value = (int)(100 * (float)counter / itemCount);
+                    counter++;
+                }
+                statusLabel.Text = "Done. " + itemCount + " .trc files converted to .csv";
+            }
+            if (bvh)
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    ConvertBVHToCSV(item.ToString());
+                    toolStripProgressBar1.ProgressBar.Value = (int)(100 * (float)counter / itemCount);
+                    counter++;
+                }
+                statusLabel.Text = "Done. " + itemCount + " .bvh files converted to .csv";
+            }
+            
+        }
+        private string ConvertStringArrayToLine(string[] array)
+        {
+            string rowToWrite = "";
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                rowToWrite += array[i] + ";";
+            }
+            rowToWrite += array[array.Length - 1];
+            return rowToWrite;
+        }
+        /// <summary>
+        /// Converts .trc file to .csv, trc file has to be recorded with 
+        /// 53 markers. For other marker setups the function needs
+        /// to be arranged.
+        /// </summary>
+        /// <param name="trcFile">file name of the trc file, without folder path</param>
+        private void ConvertToCSV(string trcFile)
+        {
+            using (TextReader tr = new StreamReader(path +"\\" + trcFile))
+            {
+                string text = tr.ReadToEnd();
+                string[] rows = text.Split("\n");
+
+               
+                var header = rows[3].Split("\t");
+                var header2 = rows[4].Split("\t");
+                var values = rows[5].Split("\t");
+                //First index: First marker x position index
+                //last index: Last marker z position index
+                int firstIndex = 119;
+                int lastIndex = 277;
+                int markerCount = 53;
+                int counter = 0;
+                string[] headerLine = new string[markerCount * 3];
+                for (int i = firstIndex; i < lastIndex+1; i++)
+                {
+                    if (counter%3==0)
+                    {
+                        headerLine[counter] = header[i]+"_X";
+                    }
+                    else if (counter%3==1)
+                    {
+                        headerLine[counter] = header[i - 1] + "_Y";
+                    }
+                    else
+                        headerLine[counter] = header[i - 2] + "Z";
+                    
+                    counter++;
+                }
+                using (TextWriter tw = new StreamWriter(path + "\\" + trcFile.Replace(".trc", ".csv"))) 
+                {
+
+                    tw.WriteLine(ConvertStringArrayToLine(headerLine));
+                    for (int i = 5; i < rows.Length - 1; i++)
+                    {
+                        var vals = rows[i].Split("\t");
+                        string[] markerPositions = new string[markerCount * 3];
+                        int id = 0;
+                        
+                        
+                        for (int m = firstIndex; m < lastIndex+1; m++)
+                        {
+                            if (vals[m] == "")
+                            {
+                                markerPositions[id] = "null";
+                            }
+                            else
+                                markerPositions[id] = vals[m];
+                            id++;
+                        }
+                        
+                        
+                        tw.WriteLine(ConvertStringArrayToLine(markerPositions));
+                    }
+                    tw.Flush();
+                    tw.Close();
+                }
+                
+            }
+            
+        }
+
+        private void ConvertBVHToCSV(string bvhFile)
+        {
+            using (TextReader tr = new StreamReader(path + "\\" + bvhFile))
+            {
+                string text = tr.ReadToEnd();
+                string[] rows = text.Split("\n");
+                int totalFrames = 0;
+                int firstRowIndex = 0;
+                for (int i = 0; i < rows.Length; i++)
+                {
+                    if (rows[i].StartsWith("Frames:"))
+                    {
+                        totalFrames = int.Parse(rows[i].Split(" ")[1]);
+                        firstRowIndex = i + 2;
+                        break;
+                    }
+                }
+                string[] headerRow = new string[28 * 3];
+                headerRow[0] = "HIPS_Pos_X";
+                headerRow[1] = "HIPS_Pos_Y";
+                headerRow[2] = "HIPS_Pos_Z";
+                using (TextWriter tw = new StreamWriter(path + "\\" + bvhFile.Replace(".bvh", ".csv")))
+                {
+
+                }
+            }
+        }
+        
+    }
+}
